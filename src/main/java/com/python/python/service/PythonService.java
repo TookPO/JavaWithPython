722113f6046de97cc6c4a1aafca188637e267e04
TookPO <bavsa487@naver.com>
2024-06-25 14:30:40 +0900

    2024-6-25 14:30
    jython 기능 완성

--- src/main/java/com/python/python/service/PythonService.java
+++ src/main/java/com/python/python/service/PythonService.java
@@ -12,21 +12,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PythonService {
-	
+
 	private static PythonInterpreter pythonInterpreter;
-	
-	public List<Object> useJython(Map<String, String> dataMap){
+
+	public List<Object> useJython(Map<String, String> dataMap) {
 		pythonInterpreter = new PythonInterpreter();
 		List<Object> dataList = new ArrayList<>();
-		
+
+		// 파이썬 파일 로드
+		pythonInterpreter.execfile("src/main/resources/ListFunction.py");
+		System.out.println("Python file loaded successfully.");
+
+		// set_data 함수 가져오기
 		PyFunction pyFunction = (PyFunction) pythonInterpreter.get("set_data", PyFunction.class);
-		PyObject pyobj = pyFunction.__call__(new PyString(dataMap.get("color")), new PyString(dataMap.get("fruit")));
-		
-		for(PyObject obj : pyobj.asIterable()) {
+		if (pyFunction == null) {
+			throw new RuntimeException("Failed to read the set_data function");
+		}
+		System.out.println("set_data function loaded successfully.");
+
+		// set_data 함수 호출
+		PyObject pyobj = pyFunction.__call__(new PyString(dataMap.get("fruit")), new PyString(dataMap.get("color")));
+		for (PyObject obj : pyobj.asIterable()) {
 			dataList.add(obj.toString());
 		}
-		
+
 		return dataList;
+
 	}
-	
+
 }
